0f666ee545d754cdaedbd978bd92f794ca69a057
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-05 15:15:44 -0400

    Complted step 1 & 2

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,10 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+ axios.get("https://api.github.com/users/itava0")
+  .then(res => {
+    console.log(res);
+  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
